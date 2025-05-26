f687279c47dd9ee90ddfc5bf7e301f3f3c8e0331
Pakize-erdogan <96188710+Pakize-erdogan@users.noreply.github.com>
2025-05-26 13:17:42 +0300

    update-4

--- Build/WebBuild_Final.loader.js
+++
@@ -1,9 +0,0 @@
-function UnityProgress(gameInstance, progress) {
-    if (!gameInstance.Module)
-        return;
-
-    var container = document.getElementById("#unity-container");
-    if (progress === 1) {
-        // Yükleme bittiğinde hiçbir şey gösterme
-    }
-}
